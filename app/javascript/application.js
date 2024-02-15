495ebbfb653644999fbe295af6eebcf93f4fc69f
shahroon <shahroon.ali@gmail.com>
2024-02-15 12:36:16 +0500

    @wip

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
 document.addEventListener("turbo:load", function() {
     const steps = document.querySelectorAll('.step');
     let currentStep = 0;
