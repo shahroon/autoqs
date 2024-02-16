7dec789afff9cf31d6f7d032d1618e22733548f1
Shahzad Tariq <m.shahzad.tariq@hotmail.com>
2024-02-17 01:08:31 +0500

    fixed the modal changes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_one_attached :file
+  has_many_attached :files
 
   validates_presence_of :job_number
   validates_uniqueness_of :job_number, scope: :user_id
